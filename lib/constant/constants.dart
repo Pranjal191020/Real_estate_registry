a95bb9d90deb8254976b165a6faf491cb738f3e4
Pranjal191020 <pranjalmunot19@gmail.com>
2023-03-12 21:55:43 +0530

    changes

---
+++ lib/constant/constants.dart
@@ -0,0 +1,10 @@
+String mapBoxApiKey =
+    'pk.eyJ1Ijoic2F1cmFiaG13IiwiYSI6ImNreTRiYzNidjBhMTkydnB2dmpoeGt4ZmgifQ.2QZ4CsNiygDTAhkqASpbPg';
+
+String nftStorageApiKey =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweGEzMDQ1Mjc4MzRiQzNBNTQ1Y0I5NUQxOUI4MDNjMTcxZkI1Y2RFMjQiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY3Nzg1MjI1NjE4MywibmFtZSI6ImxhbmRfcmVnaXN0cnkifQ.HQcBi03vU6ctzK_HNtPVGkkv8G6zG5mcBW5qwzu62Z0';
+const String rpcUrl = "http://127.0.0.1:7545"; //https://goerli.infura.io/v3/24c7db91f8904c5d9aa1af1b7f66591f";
+
+const String contractAddress = "0x7dE02ad4EA07230F057d23C7d1B1900ba22748DC"; //"0x2DC5bAEd6e3971B6Bd6bE7fA5793eA783AFacDC2";
+
+const int chainId = 1337; //Polygon testnet-80001, local ganache-1337
